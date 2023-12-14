ec30a373b48fcecd89cbe9d29b344fe6569b7fab
Thuku Chris <eng.christhuku@gmail.com>
2023-12-14 23:47:12 +0300

    feat positive integer

--- HolidayingTravelCarsChallange/Program.cs
+++ HolidayingTravelCarsChallange/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Scored 100%");
+
+static int Solution(int[] P, int[] S)
+{
+    int total_pass = P.Sum();
+    int res = 0;
+    Array.Sort(S);
+    Array.Reverse(S);
+    foreach (int i in S)
+    {
+        if (total_pass - i > 0)
+        {
+            total_pass -= i;
+            res += 1;
+        }
+        else
+        {
+            res += 1;
+            return res;
+        }
+    }
+    return res;
+}
